f89bb285a38e5ccdc21d7ef1bffaf14f1fa81dfb
Vijay Gupta <vgupta8@charlotte.edu>
2026-06-21 20:48:28 -0400

    Update S3 paths in Glue ETL script

--- Glue ETL Script.py
+++ Glue ETL Script.py
@@ -14,9 +14,9 @@ spark = glueContext.spark_session
 job = Job(glueContext)
 
 # --- Define S3 Paths (Updated with your new names) ---
-s3_input_path = "s3://handsonfinallanding/"
-s3_processed_path = "s3://handsonfinalprocessed/processed-data/"
-s3_analytics_path = "s3://handsonfinalprocessed/Athena Results/"
+s3_input_path = "s3://handsonfinallanding-vijay2026-552357224949-us-east-2-an/"
+s3_processed_path = "s3://handsonfinalprocessed-vijay2026-552357224949-us-east-2-an/processed-data/"
+s3_analytics_path = "s3://handsonfinalprocessed-vijay2026-552357224949-us-east-2-an/Athena Results/"
 
 # --- Read the data from the S3 landing zone ---
 dynamic_frame = glueContext.create_dynamic_frame.from_options(
